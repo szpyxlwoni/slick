9f5c2fa094bb3b9eae865c60aa7e72f9c3f18673
Stefan Zeiger <szeiger@novocode.com>
2014-04-02 20:42:02 +0200

    Improve OSGi integration:
    - Turn down logging for the container and the service log. The actual
      test logs are unaffected.
    
    - Remove osgiTest project from root aggregation. It is not published
      and tests are not run by default. They are still run as part of
      testAll.
    
    - Add H2 driver to OSGi test and run an actual database test.
    
    - Rearrange package structure.

--- osgi-tests/src/test/scala/scala/slick/osgi/test/BasicTest.scala
+++ osgi-tests/src/test/scala/scala/slick/osgi/test/BasicTest.scala
@@ -1,30 +1,24 @@
-package scala.slick
+package scala.slick.osgi.test
 
-import org.junit.Assert._
-import org.ops4j.pax.exam.CoreOptions._
- 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Assert._
 import org.ops4j.pax.exam
-import org.ops4j.pax.exam.junit.{
-  Configuration,
-  ExamReactorStrategy,
-  JUnit4TestRunner
-}
+import org.ops4j.pax.exam.junit.{Configuration, ExamReactorStrategy, JUnit4TestRunner}
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory
-import org.ops4j.pax.swissbox.framework.ServiceLookup
-import org.osgi.framework.BundleContext
 import scala.slick.SlickException
+import scala.slick.osgi.testutil._
 
 @RunWith(classOf[JUnit4TestRunner])
 @ExamReactorStrategy(Array(classOf[AllConfinedStagedReactorFactory]))
-class BasicTest extends testutil.SlickOsgiHelper {
+class BasicTest extends SlickOsgiHelper {
 
   @Configuration
   def config(): Array[exam.Option] = {
     standardOptions
   }
- 
+
+  /*
   @Test
   def everythingLoads(): Unit = println("Running PAX-Exam test.")
 
@@ -32,5 +26,14 @@ class BasicTest extends testutil.SlickOsgiHelper {
   @Test(expected=classOf[SlickException])  
   def canDoSomethingSlick(): Unit = {
     throw new SlickException("Test failure")
+  }
+  */
+
+  @Test
+  def testLiftedEmbedding: Unit = {
+    import scala.slick.driver.H2Driver.simple._
+    Database.forURL("jdbc:h2:mem:test-osgi") withSession { implicit session =>
+      assertEquals("TEST-OSGI", Functions.database.run)
+    }
   }
 }
